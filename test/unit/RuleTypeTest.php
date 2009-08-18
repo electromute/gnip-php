3b6937f74d1e39497d4e8192e174754d992c70df
Ingrid Alongi <ingrid@gnipcentral.com>
2009-08-18 10:59:53 -0600

    Updated schema document and added better test coverage to test for out of synch schema.

--- test/unit/RuleTypeTest.php
+++ test/unit/RuleTypeTest.php
@@ -11,6 +11,39 @@ class RuleTypeTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected_xml, $rule->toXML());
     }
+    
+    
+    /**
+    * @expectedException PHPUnit_Framework_Error
+    */
+    function testToXmlValidateSchemaError()
+    {
+        // blah is not in the schema
+        $expected_xml = '<publisher name="test"><supportedRuleTypes><type>action</type><type>actor</type><type>tag</type><type>to</type><type>regarding</type><type>source</type><type>blah</type></supportedRuleTypes></publisher>';
+        $doc = new DOMDocument();
+        $doc->loadXML($expected_xml);
+        $doc->saveXML();        
+        $doc->schemaValidate(dirname(__FILE__) . '../../../src/Services/Gnip/gnip.xsd');
+    }
+    
+
+    function xtestToXmlValidateSchemaLive()
+    {
+        // makes sure the schema documents match
+        $expected_xml = '<publisher name="test"><supportedRuleTypes><type>action</type><type>actor</type><type>tag</type><type>to</type><type>regarding</type><type>source</type><type>keyword</type></supportedRuleTypes></publisher>';
+        $doc = new DOMDocument();
+        $doc->loadXML($expected_xml);
+        $doc->saveXML();        
+        $doc->schemaValidate('https://api-v21.gnip.com/schema/gnip.xsd');
+    }
+    
+    function testToXmlValidateSchemaLive()
+    {
+        // makes sure the schema documents match
+        $local_doc = file_get_contents(dirname(__FILE__) . '../../../src/Services/Gnip/gnip.xsd');
+        $remote_doc = file_get_contents('https://api-v21.gnip.com/schema/gnip.xsd');
+        $this->assertEquals($local_doc, $remote_doc);
+    }
 
     function testFromXml()
     {
